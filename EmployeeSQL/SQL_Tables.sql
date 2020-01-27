7248951df6b9e64c1958ca2b0bfbf86b74dae242
unknown <shidalgo1212@gmail.com>
2020-01-27 01:13:10 -0600

    Create Tables

---
+++ EmployeeSQL/SQL_Tables.sql
@@ -0,0 +1,42 @@
+--Create Tables
+create table departments(
+	dept_no varchar,
+	dept_name varchar
+);
+
+create table dept_emp(
+	emp_no INT,
+	dept_no varchar,
+	from_date DATE,
+	to_date DATE
+);
+
+create table dept_manager(
+	dept_no varchar,
+	emp_no INT,
+	from_date DATE,
+	to_date DATE
+);
+
+create table employees(
+	emp_no INT,
+	birth_date DATE,
+	first_name varchar,
+	last_name varchar,
+	gender varchar,
+	hire_date DATE
+);
+
+create table salaries(
+	emp_no INT,
+	salary INT,
+	from_date DATE,
+	to_date DATE
+);
+
+create table titles(
+	emp_no INT,
+	title varchar,
+	from_date DATE,
+	to_date DATE
+);
